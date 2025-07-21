8c246faaace3ebb6e0fe95d9c007a7f7aa6035b7
ValentinaSanchezMelchor <valentinasmtr333@gmail.com>
2025-07-21 11:51:27 +0200

    Add working test script for S-Information using JIDT

---
+++ src/info_theory/jidt_interface/tests/test_jidt_sinfo.py
@@ -0,0 +1,32 @@
+import jpype
+import jpype.imports
+from jpype.types import JArray, JDouble
+import numpy as np
+import os
+
+os.environ['OMP_NUM_THREADS'] = '1'
+
+jidt_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "infodynamics.jar"))
+
+if not jpype.isJVMStarted():
+    jpype.startJVM(classpath=[jidt_path])
+
+# Create S-information calculator
+SInfoCalc = jpype.JClass("infodynamics.measures.continuous.gaussian.SInfoCalculatorGaussian")
+sinfo = SInfoCalc()
+
+# Dummy data: 5 observations, 3 variables
+X = np.array([
+    [1.0, 2.0, 3.0],
+    [1.1, 2.1, 3.1],
+    [0.9, 1.8, 3.2],
+    [1.2, 2.2, 3.0],
+    [0.8, 2.0, 3.3]
+])
+
+sinfo.initialise(X.shape[1])  # Only number of variables
+sinfo.setObservations(JArray(JDouble, 2)(X.tolist()))
+value = sinfo.computeAverageLocalOfObservations()
+print("S-Information:", value)
+
+jpype.shutdownJVM()
